7d016e48f23125f9bba7da6fc7fb2b18db9ebc90
Niels de Blaauw <niels@level-level.com>
2018-12-12 12:23:04 +0100

    Initial commit

---
+++ grasp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Grasp
+ * Plugin URI:      https://grasp.actd.nl
+ * Description:     Making usable and accessible websites that are found well should be easy. Grasp helps you optimise WordPress images in a way that makes the web better.
+ * Author:          Niels de Blaauw
+ * Author URI:      https://actd.nl
+ * Text Domain:     grasp
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Grasp
+ */
+
+// Your code starts here.
---
+++ readme.txt
@@ -0,0 +1,11 @@
+=== Grasp ===
+Contributors: nielsdeblaauw
+Donate link: https://actd.nl/donate
+Tags: image, accessibility, seo, javascript, api, admin, development, content
+Requires at least: 4.5
+Tested up to: 5.0
+Stable tag: 0.1.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Making usable and accessible websites that are found well should be easy. Grasp helps you optimise WordPress images in a way that makes the web better.
